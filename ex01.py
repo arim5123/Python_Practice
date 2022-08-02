efb898217c9742cdd5dadda7777e19fcb11ad300
arim5123 <109630259+arim5123@users.noreply.github.com>
2022-08-02 21:51:34 +0900

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -9,22 +9,24 @@ price_url = "https://api.upbit.com/v1/candles/minutes/1"
 name_response = requests.get(name_url, headers=headers)
 price__response = requests.get(price_url, headers=headers)
 
+con = pymysql.connect(host='localhost', user='root', password='1234', db='python_test', charset='utf8')
+cur = con.cursor()
+
 ndata = name_response.json()
 pdata = price__response.json()
 
 for i in range(0,len(ndata)) :
-    print(ndata[i]['korean_name'])
+    name_val_01 = str(ndata[i]['korean_name'])
+    name_val_02 = str(ndata[i]['english_name'])
+    sql = "INSERT INTO subject_name(k_name, e_name) VALUES (name_val_01, name_val_02);"
+    cur.execute(sql)
+    rows = cur.fetchall()
 
+select_sql = "SELECT * FROM subject_name"
+cur.execute(select_sql)
+select_rows = cur.fetchall()
 
-con = pymysql.connect(host='localhost', user='root', password='Arim1004!', db='ei4db', charset='utf8')
-cur = con.cursor()
-
-sql = "SELECT * FROM account"
-
-cur.execute(sql)
-rows = cur.fetchall()
-
-for i in rows :
+for i in select_rows :
     print(i)
 
 con.close()
