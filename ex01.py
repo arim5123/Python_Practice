cfc5178be69c91e254480f7baba50aa1e104aa87
arim5123 <109630259+arim5123@users.noreply.github.com>
2022-08-03 16:58:11 +0900

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -9,7 +9,7 @@ price_url = "https://api.upbit.com/v1/candles/minutes/1"
 name_response = requests.get(name_url, headers=headers)
 price__response = requests.get(price_url, headers=headers)
 
-con = pymysql.connect(host='localhost', user='root', password='1234', db='python_test', charset='utf8')
+con = pymysql.connect(host='localhost', user='root', password='Arim1004!', db='python_test', charset='utf8')
 cur = con.cursor()
 
 ndata = name_response.json()
@@ -19,11 +19,10 @@ for i in range(0,len(ndata)) :
     num = str(i+1)
     name_val_01 = str(ndata[i]['korean_name'])
     name_val_02 = str(ndata[i]['english_name'])
-    sql = "INSERT INTO subject_name VALUES (%s, %s, %s)"
-    val = (num, name_val_01, name_val_02)
-    cur.execute(sql, val)
 
-    result = rows = cur.fetchall()
-    print(result)
+    sql = "INSERT INTO subject_name VALUES (%s, %s)"
+    val = (name_val_01, name_val_02)
+    cur.execute(sql, val)
+    con.commit()
 
 con.close()
